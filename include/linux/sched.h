39a5a54370bbb9bdd196c32057fc492728494282
jollaman999 <admin@jollaman999.com>
2016-07-25 16:11:19 +0900

    sched.h: Fix merge derp

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1363,8 +1363,6 @@ struct task_struct {
 #endif
 	unsigned long atomic_flags; /* Flags needing atomic access. */
 
-	unsigned long atomic_flags; /* Flags needing atomic access. */
-
 	pid_t pid;
 	pid_t tgid;
 
@@ -1877,6 +1875,8 @@ extern int task_free_unregister(struct notifier_block *n);
 
 /* Per-process atomic flags. */
 #define PFA_NO_NEW_PRIVS 0x00000001	/* May not gain new privileges. */
+#define PFA_SPREAD_PAGE  1      /* Spread page cache over cpuset */
+#define PFA_SPREAD_SLAB  2      /* Spread some slab caches over cpuset */
 
 static inline bool task_no_new_privs(struct task_struct *p)
 {
